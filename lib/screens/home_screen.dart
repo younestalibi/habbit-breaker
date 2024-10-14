5cce5e4aca584385a776fa1211e3819318434ab5
Younes Talibi <younessetalibi11@gmail.com>
2024-10-14 23:44:28 +0100

    add drawer side menu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:habbit_breaker/providers/auth_provider.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:provider/provider.dart';
-import '../providers/auth_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PersistentTabController? _controller;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      Center(child: Text("Home Page")),
+      Center(child: Text("Search Page")),
+      Center(child: Text("Profile Page")),
+      Center(child: Text("Community Page")),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.home),
+        title: "Home",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.search),
+        title: "Search",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.person),
+        title: "Profile",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.people),
+        title: "Community",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey, // Attach the key to the Scaffold
+
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('My App'),
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
@@ -17,8 +72,80 @@ class HomeScreen extends StatelessWidget {
             },
           ),
         ],
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        ),
       ),
-      body: Center(child: Text('Welcome to the Home Screen!')),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Material(
+                color: Colors.blueAccent,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(50),
+                    child: Column(
+                      children: const [
+                        CircleAvatar(
+                          radius: 52,
+                          backgroundImage: NetworkImage(
+                              'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c21pbHklMjBmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                          'Younes',
+                          style: TextStyle(fontSize: 28, color: Colors.white),
+                        ),
+                        const Text(
+                          'younes@gmail.com',
+                          style: TextStyle(fontSize: 14, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.home_outlined),
+                    title: Text('Home'),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, '/home');
+                    },
+                  ),
+                  const Divider(
+                    color: Colors.black45,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.notifications_outlined),
+                    title: Text('Notifications'),
+                    onTap: () {},
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      body: PersistentTabView(
+        context,
+        controller: _controller!,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        navBarStyle: NavBarStyle.style9,
+      ),
     );
   }
 }
